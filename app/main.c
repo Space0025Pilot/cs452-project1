1fb72f9d45fb1f1fda8137049bbe08a39b268207
Space0025Pilot <111655154+Space0025Pilot@users.noreply.github.com>
2024-09-16 22:05:14 +0000

    Task 3 Complete, ASAN Problem

--- app/main.c
+++ app/main.c
@@ -8,38 +8,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "lab.h"
+#include <getopt.h>
+// #include <readline/readline.h>
+// #include <readline/history.h>
 
-int main (int argc, char **argv)
+#include "../src/lab.h"
+
+int main(int argc, char **argv)
 {
-  printf("hello world\n");
 
-  int c;
+  int opt;
 
-  while ((c = getopt (argc, argv, "abc:")) != -1)
-    switch (c)
-      {
-      case 'a':
-        printf("get a here");
-        break;
-      case 'b':
-        printf("get b here");
-        break;
-      case 'c':
-        printf("get c here");
-        break;
-      case '?':
-        if (isprint(optopt))
-          fprintf (stderr, "Unknown option", optopt);
-        else
-          fprintf (stderr, "Unknown option", optopt);
-        return 1;
-      default:
-        abort ();
-        break;
-      }
+  /* Use getopt() to process command line arguments */
+  while ((opt = getopt(argc, argv, "avczh")) != -1)
+  {
+    switch (opt)
+    {
+    case 'a':
+      printf("Get a here");
+      break;
+    case 'v':
+      printf("Version -> %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+      break;
+    case 'c':
+      printf("Hi c");
+      break;
+    case 'z':
+      printf("Hi z");
+      break;
+    case 'h':
+      // fprintf(stdout, "Usage: %s [-a] [-v] [-c] [-z] [-h]\n", argv[0]);
+      fprintf(stdout, "        -a        Print statement\n");
+      fprintf(stdout, "        -v        Display the shell version\n");
+      fprintf(stdout, "        -c        Display proc entries sorted by command lexicographically");
+      fprintf(stdout, "        -z        Display ONLY proc entries in the zombie state");
+      fprintf(stdout, "        -h        Display this help message");
+      exit(0);
+    // default:
+    //     fprintf(stderr, "Error: Invalid Option Specified\n");
+    //     // fprintf(stderr, "Usage: %s [-d <path>] \n", argv[0]);
+    //     break;
+    case '?':
+      if (optopt == 'opt')
+        fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+      else if (isprint(optopt))
+        fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+      else
+        fprintf(stderr,
+                "Unknown option character `\\x%x'.\n",
+                optopt);
+      return 1;
+    default:
+      abort();
+    }
+  }
   return 0;
 }
+
+
+
 // #include <stdio.h>
 
 // int main(void)
--- src/lab.c
+++ src/lab.c
@@ -4,14 +4,16 @@
  * Description: Definition of support functions used in main.c
  **/
 
-#include <stdlib.h>
-#include <stdbool.h>
-#include <sys/types.h>
-#include <termios.h>
-#include <unistd.h>
+// #include <stdlib.h>
+// #include <stdbool.h>
+// #include <sys/types.h>
+// #include <termios.h>
+// #include <unistd.h>
+#include <string.h>
 #include "lab.h"
+#include "../tests/harness/unity.h"
 
-char *get_prompt(const char *env);
+// char *get_prompt(const char *env);
 
 
 
