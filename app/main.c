bfd5fe3ace5d748a4bf683cc88ad0965a3a2ed3a
Space0025Pilot <111655154+Space0025Pilot@users.noreply.github.com>
2024-09-17 17:08:24 +0000

    Continuing work on Task 5

--- app/main.c
+++ app/main.c
@@ -9,8 +9,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
-// #include <readline/readline.h>
-// #include <readline/history.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include "../src/lab.h"
 
@@ -20,29 +20,29 @@ int main(int argc, char **argv)
   int opt;
 
   /* Use getopt() to process command line arguments */
-  while ((opt = getopt(argc, argv, "avczh")) != -1)
+  while ((opt = getopt(argc, argv, "pvczh")) != -1)
   {
     switch (opt)
     {
-    case 'a':
-      printf("Get a here");
+    case 'p':
+      printf("Get prompt variable: %s\n", get_prompt("MY_PROMPT"));
       break;
     case 'v':
       printf("Version -> %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
       break;
     case 'c':
-      printf("Hi c");
+      printf("Hi c\n");
       break;
     case 'z':
-      printf("Hi z");
+      printf("Hi z\n");
       break;
     case 'h':
       // fprintf(stdout, "Usage: %s [-a] [-v] [-c] [-z] [-h]\n", argv[0]);
-      fprintf(stdout, "        -a        Print statement\n");
+      fprintf(stdout, "        -p        Prints shell prompt\n");
       fprintf(stdout, "        -v        Display the shell version\n");
-      fprintf(stdout, "        -c        Display proc entries sorted by command lexicographically");
-      fprintf(stdout, "        -z        Display ONLY proc entries in the zombie state");
-      fprintf(stdout, "        -h        Display this help message");
+      fprintf(stdout, "        -c        Display proc entries sorted by command lexicographically\n");
+      fprintf(stdout, "        -z        Display ONLY proc entries in the zombie state\n");
+      fprintf(stdout, "        -h        Display this help message\n");
       exit(0);
     // default:
     //     fprintf(stderr, "Error: Invalid Option Specified\n");
--- src/lab.c
+++ src/lab.c
@@ -4,16 +4,60 @@
  * Description: Definition of support functions used in main.c
  **/
 
-// #include <stdlib.h>
-// #include <stdbool.h>
-// #include <sys/types.h>
-// #include <termios.h>
-// #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
 #include <string.h>
 #include "lab.h"
 #include "../tests/harness/unity.h"
 
-// char *get_prompt(const char *env);
+char *get_prompt(const char *env){
+    if(getenv(env) != NULL){
+        printf("Inside not null");
+        setenv(env, "foo>", 1);
+        free(getenv(env));
+        return getenv(env);   //Note here that we need to free the resulting string from getenv as it calls malloc internally.
+    }
+    if (getenv(env) == NULL){
+        printf("Inside else");
+        setenv(env, "shell>", 1);
+        return getenv(env);
+    }
+    
+    
+}
+
+void sh_init(struct shell *sh){
+
+    /* Allocate space for shell struct in heap and validate */
+    sh = (struct shell *)malloc(sizeof(struct shell));
+    // if(sh == NULL){
+    //     return NULL;
+    // }
+
+    /* Initialize the sh with the initial values */
+    sh->shell_is_interactive = 1;
+    sh->shell_pgid = 0;
+    // struct termios *tm = malloc(sizeof(struct termios));   //Not sure this is a struct so do I allocate the memory for this.
+    // sh->shell_tmodes = expression;
+    sh->shell_terminal = 1;
+    sh->prompt = "";
+}
+
+void sh_destroy(struct shell *sh){
+
+    /* Validate Parameter */
+    if(sh == NULL){
+        return;
+    }
+
+    // free(sh->shell_tmodes); //Again not sure how to do this
+
+    free(sh);
+}
 
 
 
